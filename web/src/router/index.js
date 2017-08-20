ab830add4a9607e911f5dfa73f8bb8d7cc29223f
bgpat <bgpat23@gmail.com>
2017-08-20 14:13:59 +0900

    Implement pages of detail

--- web/src/router/index.js
+++ web/src/router/index.js
@@ -2,7 +2,9 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import Root from '@/components/Root';
 import Nodes from '@/components/Nodes';
+import Node from '@/components/Node';
 import Images from '@/components/Images';
+import Image from '@/components/Image';
 
 Vue.use(Router);
 
@@ -19,9 +21,19 @@ export default new Router({
       component: Nodes,
     },
     {
+      path: '/nodes/:id',
+      name: 'Node',
+      component: Node,
+    },
+    {
       path: '/images',
       name: 'Images',
       component: Images,
+    },
+    {
+      path: '/images/:id',
+      name: 'Image',
+      component: Image,
     },
   ],
 });
