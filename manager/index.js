4f2a6048e4ac6547deddd15bf447aa3ce35da074
bgpat <bgpat23@gmail.com>
2017-08-20 11:50:18 +0900

    Add CORS header

--- manager/index.js
+++ manager/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 
 app.use(bodyParser.json());
+app.use(cors());
 app.use(require('./db'));
 app.use(require('./controllers'));
 
