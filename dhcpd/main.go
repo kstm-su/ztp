14fff3866c9b6c52633eb69a6ffa6a5b48312d4a
bgpat <bgpat23@gmail.com>
2017-08-20 22:13:54 +0900

    Implement node type

--- dhcpd/main.go
+++ dhcpd/main.go
@@ -8,11 +8,27 @@ import (
 	"github.com/parnurzeal/gorequest"
 )
 
+type node struct {
+	id         int
+	name       string
+	macAddress string `json:"mac_address"`
+	ipAddress  string `json:"ip_address"`
+	image      image
+}
+
+type image struct {
+	id          int
+	path        string
+	config      string
+	name        string
+	description string
+}
+
 func main() {
 	apiURL := os.Getenv("API_URL")
 	s, err := server.New(func(lease *server.Lease) server.Reply {
 		fmt.Printf("lease: %+v\n", lease)
-		nodes := []map[string]string{}
+		nodes := []node{}
 		_, _, err := gorequest.New().Get(apiURL + "/nodes").EndStruct(&nodes)
 		if err != nil {
 			fmt.Println(err)
@@ -20,7 +36,7 @@ func main() {
 		}
 		macAddr := lease.CHAddr.String()
 		for _, node := range nodes {
-			if node["mac_address"] == macAddr {
+			if node.macAddress == macAddr {
 				fmt.Printf("node: %+v\n", node)
 				return nil
 			}
