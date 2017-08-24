4d7bc590e6aae31aedf1898fa7eb1d003cff43e6
Yuta K <orn_tk@me.com>
2017-08-24 20:35:07 +0900

    Sharing image file with TFTP Server

--- moby/main.go
+++ moby/main.go
@@ -2,11 +2,15 @@ package main
 
 import (
 	"bytes"
+	"crypto/md5"
+	"encoding/hex"
 	"log"
 
 	"github.com/gin-gonic/gin"
 	"github.com/moby/tool/src/moby"
 )
+
+const outputDir = "/tftpboot/"
 
 func main() {
 	router := gin.Default()
@@ -17,6 +21,11 @@ func main() {
 		log.Printf("%v", c.PostForm("config"))
 		config := []byte(c.PostForm("config"))
 
+		// Create Filename Hash
+		h := md5.New()
+        h.Write([]byte(config))
+		filename := outputDir + hex.EncodeToString(h.Sum(nil))
+
 		m, err := moby.NewConfig(config)
 		if err != nil {
 			log.Fatalf("Invalid config: %v", err)
@@ -26,12 +35,12 @@ func main() {
 			log.Fatalf("%v", err)
 		}
 		image := buf.Bytes()
-		if err := moby.Outputs("/tmp/tftpboot/output", image, []string{"iso-bios", "iso-efi"}, 1024, false); err != nil {
+		if err := moby.Outputs(filename, image, []string{"iso-bios", "iso-efi"}, 1024, false); err != nil {
 			log.Fatalf("Error writing outputs: %v", err)
 		}
 
 		c.JSON(200, gin.H{
-			"path": "/tftpboot/output.iso",
+			"path": filename + ".iso",
 		})
 	})
 	router.Run(":3000")
