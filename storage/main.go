1c9f748a680d3976f551314488e3bc145a79b6e7
bgpat <bgpat23@gmail.com>
2017-08-26 22:24:48 +0900

    Add queue to build image

--- storage/main.go
+++ storage/main.go
@@ -10,7 +10,6 @@ import (
 
 func postImage(c *gin.Context) {
 	image := Image{}
-	log.Printf("[DEBUG] image: %+v\n", image)
 	if err := c.Bind(&image); err != nil {
 		log.Printf("[ERROR] %v\n", err)
 		c.JSON(http.StatusBadRequest, gin.H{
@@ -18,13 +17,8 @@ func postImage(c *gin.Context) {
 		})
 		return
 	}
-	if err := image.Build(); err != nil {
-		log.Printf("[ERROR] %v\n", err)
-		c.JSON(http.StatusInternalServerError, gin.H{
-			"error": err.Error(),
-		})
-		return
-	}
+	log.Printf("[DEBUG] form: %+v\n", image)
+	image.Append()
 	c.JSON(http.StatusOK, image)
 }
 
