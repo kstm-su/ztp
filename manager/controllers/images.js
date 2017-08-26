7db1ea4dce982d1dcc22880e323ee1741ccb1dd3
bgpat <bgpat23@gmail.com>
2017-08-27 01:20:03 +0900

    Update image path

--- manager/controllers/images.js
+++ manager/controllers/images.js
@@ -1,18 +1,21 @@
 const express = require('express');
+const axios = require('axios');
 const models = require('../models');
 
 const router = express.Router();
+const url = `${process.env.STORAGE_URL}/images`;
 
 router.get('/', (req, res) => {
   models.Images.findAll().then(images => res.json(images));
 });
 
 router.post('/', (req, res, next) => {
-  models.Images.create(req.body).then(image => {
-    res.json(image.get({
-      plain: true,
-    }));
-  }).catch(err => next(err));
+  models.Images.create(req.body)
+    .then(image => image.get({ plain: true }))
+    .then(image => axios.post(url, image))
+    .then(resp => resp.data)
+    .then(image => res.json(image))
+    .catch(err => next(err));
 });
 
 router.get('/:id', (req, res, next) => {
@@ -26,13 +29,16 @@ router.get('/:id', (req, res, next) => {
   });
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', (req, res, next) => {
   models.Images.findById(req.params.id).then(image => {
     if (image == null) {
-      return next();
+      throw new Error('image is null');
     }
-    image.update(req.body).then(image => res.json(image)).catch(err => next(err));
-  });
+    return image.update(req.body);
+  }).then(image => req.body.build ? axios(url, image) : { data: image })
+    .then(resp => resp.data)
+    .then(image => res.json(image))
+    .catch(err => next(err));
 });
 
 module.exports = router;
