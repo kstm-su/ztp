0a1b5dd54e8f459f9e36c48321c40c2d0ee817d7
bgpat <bgpat23@gmail.com>
2017-08-27 12:36:20 +0900

    Replace with absolute path

--- manager/entrypoint.sh
+++ manager/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 rm -rf $SOCKET
 if ! [ -d "node_modules/$NODE_MODULE" ]; then
-	yarn add $NODE_MODULE
+	/usr/local/bin/yarn add $NODE_MODULE
 fi
-node index.js
+/usr/local/bin/node index.js
