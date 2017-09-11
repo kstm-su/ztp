1019dc48a3d1e18cec4a507e3adac993427b6343
bgpat <bgpat23@gmail.com>
2017-09-11 23:57:50 +0900

    Change output directory

--- manager/webpack/webpack.config.js
+++ manager/webpack/webpack.config.js
@@ -5,7 +5,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, './dist'),
+    path: path.resolve(__dirname, '../static'),
     filename: 'bundle.js',
   },
   plugins: [
