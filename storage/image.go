bd1f317f8d75bf9f47e49f68e6cddd94ebee8986
bgpat <bgpat23@gmail.com>
2017-09-14 11:34:12 +0900

    Omit remove exist directory

--- storage/image.go
+++ storage/image.go
@@ -65,9 +65,9 @@ func (i *Image) Build() error {
 	}
 	path := filepath.Join(outputDir, strconv.Itoa(i.ID))
 
-	if err := os.RemoveAll(path); err != nil && !os.IsNotExist(err) {
-		return err
-	}
+	// if err := os.RemoveAll(path); err != nil && !os.IsNotExist(err) {
+	// 	return err
+	// }
 
 	if err := os.MkdirAll(path+"/syslinux/pxelinux.cfg", 0755); err != nil {
 		return err
